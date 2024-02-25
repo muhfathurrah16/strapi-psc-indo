0bd177233128c45e0859c151d5de57b3ecad80e8
muhfathurrah16 <muhammadfathur88@gmail.com>
2024-02-25 21:10:51 +0700

    first ini

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,3 @@
-// module.exports = ({ env }) => ({
-//     "vercel-deploy": {
-//       enabled: true,
-//     },
-//   });
 module.exports = ({ env }) => ({
     "vercel-deploy": {
         enabled: true,
