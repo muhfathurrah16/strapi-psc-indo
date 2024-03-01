fb00561ab60bde08719c59c189c051fc687c816f
muhfathurrah16 <muhammadfathur88@gmail.com>
2024-03-02 01:06:51 +0700

    remove vercel

--- config/plugins.js
+++ config/plugins.js
@@ -1,12 +1,1 @@
-module.exports = ({ env }) => ({
-    "vercel-deploy": {
-        enabled: true,
-        config: {
-            deployHook:
-                "https://api.vercel.com/v1/integrations/deploy/prj_xcdcfW1Ayjr0WHI5tTpatFMroAPp/iKSn8lZiRP",
-            apiToken: "oFaPzpoWQRBpoNIoaPQch3ky",
-            appFilter: "psc-indo-strapi",
-            roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
-        },
-    },
-});
+module.exports = ({ env }) => ({});
